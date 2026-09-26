f9064d8580c10f10987320db03fc968ef0820c5e
emergencese-droid <emergencese@gmail.com>
2026-09-26 08:24:48 +0200

    Create ResourceProvider.js

---
+++ backend/backend/providers/ResourceProvider.js
@@ -0,0 +1,17 @@
+export class ResourceProvider {
+  async search(query) {
+    throw new Error("Not implemented");
+  }
+
+  async get(id) {
+    throw new Error("Not implemented");
+  }
+
+  async save(resource) {
+    throw new Error("Not implemented");
+  }
+
+  async delete(id) {
+    throw new Error("Not implemented");
+  }
+}
